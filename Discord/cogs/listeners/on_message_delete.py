574321bca291ed7a6608e02628c69185c9ed8c88
Mister-SOSA <karabetsaki@gmail.com>
2023-11-06 17:38:40 +0000

    added "on_message_delete" listener for mods

---
+++ Discord/cogs/listeners/on_message_delete.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+from common.utils.configutil import fetch_convar
+
+MODLOG_CHANNEL_ID = fetch_convar("MODLOG_CHANNEL_ID")
+
+class OnMessageDelete(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_message_delete(self, msg):
+        channel = self.client.get_channel(MODLOG_CHANNEL_ID)
+        
+        embed = discord.Embed(
+            title="🗑️ Message Deleted",
+            description=f"Message deleted in {msg.channel.mention}",
+            color=discord.Color.red()
+        )
+        
+        embed.add_field(name="Message Author", value=msg.author.mention)
+        embed.add_field(name="Message Content", value=msg.content)
+        
+        await channel.send(embed=embed)
+
+async def setup(client):
+    await client.add_cog(OnMessageDelete(client))
