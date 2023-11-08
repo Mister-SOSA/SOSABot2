029f75b5291d269a7f3e3c42cd4be5a24482b302
Mister-SOSA <karabetsaki@gmail.com>
2023-11-08 21:46:15 +0000

    added kick event to modlog

--- Discord/cogs/listeners/on_member_remove.py
+++ Discord/cogs/listeners/on_member_remove.py
@@ -12,6 +12,17 @@ class OnMemberRemove(commands.Cog):
     async def on_member_remove(self, member):
         channel = self.client.get_channel(MODLOG_CHANNEL_ID)
         
+        async for log in member.guild.audit_logs(limit=1, action=discord.AuditLogAction.kick):
+            if log.target.id == member.id:
+                embed = discord.Embed(
+                    title="👢 User Kicked",
+                    description=f"User kicked: {member.name}\nKicked by: {log.user.mention}",
+                    color=discord.Color.red()
+                )
+                
+                await channel.send(embed=embed)
+                return
+        
         embed = discord.Embed(
             title="👋 User Left",
             description=f"User left: {member.name}",
